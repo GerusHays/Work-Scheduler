c4f2d20e03ef7a06d05326ad6dd1a3b902cc964b
Hays4Days <gerushays@gmail.com>
2022-04-17 19:28:12 -0700

    html restructure

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,17 @@ $(document).ready(function(){
     $("#currentDay").text(moment().format('MMMM Do YYYY'));
     // on click function save whatever you input into the field and time 
     $(".save").on("click", function(){
+        // displays message to the user upon saving their event to notify them they successfully saved their event to localStorage
+        $("#storeMessage").removeClass("hidden");
         // using this function to display the variables and then save it into the local storage 
         var text = $(this).siblings(".text").val();
         
         var time = $(this).parent().attr("id");
         // sets time and text to local storage
         localStorage.setItem(time, text);
-    })
+    });
+
+    
     // gets the text and time from localStorage and saves it so it doesnt go away after you hit refresh
     $("#09AM .text").val(localStorage.getItem("09AM"));
     $("#10AM .text").val(localStorage.getItem("10AM"));
